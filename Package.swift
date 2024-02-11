9c5083859651c34ea0553afa1ded1de24722b83c
Abe White <abewhite@mac.com>
2024-02-10 23:35:00 -0600

    Update SkipFoundation version requirement

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "0.8.7"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.5.1"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.5.2"),
     ],
     targets: [
         .target(name: "SkipModel", dependencies: [.product(name: "SkipFoundation", package: "skip-foundation")], plugins: [.plugin(name: "skipstone", package: "skip")]),
