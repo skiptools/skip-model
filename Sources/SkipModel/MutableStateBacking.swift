dab9da5e08a7710f10a0609a0d4b152997ed4990
Abe White <abewhite@mac.com>
2025-01-01 23:32:49 -0600

    Fix initial native @Observable changes not triggering recompose

--- Sources/SkipModel/MutableStateBacking.swift
+++ Sources/SkipModel/MutableStateBacking.swift
@@ -9,7 +9,8 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
 public final class MutableStateBacking: StateTracker {
-    private var state: MutableList<MutableState<Int>>?
+    private var state: MutableList<MutableState<Int>> = mutableListOf()
+    private var isTracking = false
 
     public init() {
         StateTracking.register(self)
@@ -17,31 +18,31 @@ public final class MutableStateBacking: StateTracker {
 
     public func access(stateAt index: Int) {
         synchronized(self) {
-            if let state {
-                while state.size <= index {
-                    state.add(mutableStateOf(0))
-                }
-                let _ = state[index].value
-            }
+            initialize(stateAt: index)
+            let _ = state[index].value
         }
     }
 
     public func update(stateAt index: Int) {
         synchronized(self) {
-            if let state {
-                while state.size <= index {
-                    state.add(mutableStateOf(0))
-                }
+            initialize(stateAt: index)
+            // Only update state when tracking. We do, however, read state even when tracking has not begun.
+            // Otherwise post-tracking updates may not cause recomposition
+            if isTracking {
                 state[index].value += 1
             }
         }
     }
 
+    private func initialize(stateAt index: Int) {
+        while state.size <= index {
+            state.add(mutableStateOf(0))
+        }
+    }
+
     public func trackState() {
         synchronized(self) {
-            if state == nil {
-                state = mutableListOf()
-            }
+            isTracking = true
         }
     }
 }
